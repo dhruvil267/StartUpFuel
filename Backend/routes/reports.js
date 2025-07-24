33488e62f0509a4d9563af85a4d806ed5fd2742e
Dhruvil Patel <patel2dhruvil202@gmail.com>
2025-07-24 11:50:45 -0400

    report for ec2

--- Backend/routes/reports.js
+++ Backend/routes/reports.js
@@ -6,9 +6,9 @@ const AWS = require("aws-sdk");
 
 // Configure AWS
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION || 'ca-central-1',
+  // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // region: process.env.AWS_REGION,
 });
 const router = express.Router();
 
